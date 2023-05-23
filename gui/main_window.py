13df04307dc6f3d1d251811bdcc7ef5d7362c8c4
h2magic <h2magic@163.com>
2023-05-23 17:38:10 +0800

    using timer for update once second

---
+++ gui/main_window.py
@@ -0,0 +1,16 @@
+from PyQt5.QtCore import QTimer, pyqtSignal
+from PyQt5.QtWidgets import QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.timer = QTimer(self)
+        self.tick = 1
+
+        self.timer.timeout.connect(self.receiver_from_server)
+        self.timer.start(1000)
+
+    def receiver_from_server(self):
+        print("Hello world", self.tick)
+        self.tick += 1
--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+import multiprocessing
+
+from PyQt5.QtWidgets import QApplication
+from gui.main_window import MainWindow
+
+
+def start_gui():
+    app = QApplication(sys.argv)
+
+    main_window = MainWindow()
+    main_window.show()
+
+    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
+    start_gui()
 
-    app = QApplication(sys.argv)
+# def start_queue(pipe):
+#     while True:
+#         command = pipe.recv()
+#         print("Command: ", command)
+#
+#         if command == "quit":
+#             return
 
-    w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
-    w.show()
 
-    sys.exit(app.exec_())
+# if __name__ == '__main__':
+#     queue_recv_pipe, queue_emit_pipe = multiprocessing.Pipe()
+#     ws_pipe = multiprocessing.Pipe()
+#
+#     p1 = multiprocessing.Process(target=start_queue, args=(queue_recv_pipe,))
+#     p2 = multiprocessing.Process(target=start_gui, args=(queue_emit_pipe,))
+#
+#     p1.start()
+#     p2.start()
+#
+#     p1.join()
+#     p2.join()
