768185a566c3dd6c6e03f9f101fe3e2f3c97991c
h2magic <h2magic@163.com>
2023-05-24 13:10:26 +0800

    add window for start setting

--- gui/single_setting_window.py
+++ gui/single_setting_window.py
@@ -1,22 +1,57 @@
 # from PyQt5.QtCore import QTimer
 from PyQt5 import QtGui
-from PyQt5.QtGui import QPainter
-from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QGridLayout, QPushButton, QLabel, QWidget, QLineEdit
 # from PyQt5.QtCore import
 
-from utils.settings import *
+from utils.settings import TITLE
 
 
 class SingleSettingWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.last_window = self.parent()
+        pos = self.last_window.pos()
+
         self.setWindowTitle(TITLE)
+        self.setGeometry(pos.x(), pos.y(), 400, 300)
+
+        layout = QGridLayout()
+        layout.addWidget(QLabel("世界宽度"), 0, 0)
+        layout.addWidget(QLineEdit(""), 0, 1)
+        layout.addWidget(QLabel("AI数量"), 1, 0)
+        layout.addWidget(QLineEdit(""), 1, 1)
+        layout.addWidget(QLabel("(未启用)"), 1, 2)
+        layout.addWidget(QLabel("资源比例"), 2, 0)
+        layout.addWidget(QLineEdit(""), 2, 1)
+        layout.addWidget(QLabel("国家名称"), 3, 0)
+        layout.addWidget(QLineEdit(""), 3, 1)
+        layout.addWidget(QLabel("启用AI"), 4, 0)
+        layout.addWidget(QLineEdit(""), 4, 1)
+        layout.addWidget(QLabel("(未启用)"), 4, 2)
+        layout.addWidget(QLabel("导入AI模型"), 5, 0)
+        layout.addWidget(QLineEdit(""), 5, 1)
+        layout.addWidget(QPushButton("打开文件", self), 5, 2)
+
+
+        btn_back = QPushButton("返回", self)
+        btn_back.clicked.connect(self.close_window)
+        layout.addWidget(btn_back, 6, 2)
+
+        layout.addWidget(QPushButton("开始", self), 7, 2)
+
+        center_widget = QWidget(self)
+        center_widget.setLayout(layout)
+        self.setCentralWidget(center_widget)
+
 
     def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
-        self.parent().show()
+        self.last_window.show()
 
         return super().closeEvent(a0)
+    
+    def close_window(self):
+        self.close()
 
     #     self.timer = QTimer(self)
     #     self.tick = 1
